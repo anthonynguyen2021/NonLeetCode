35714243102760559b95456ec8f03d477b5518be
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-11 23:15:44 -0800

    Add files via upload

---
+++ MinHeightBST/MinHeightBST.py
@@ -0,0 +1,20 @@
+# Time - O(n)
+# Space - O(n)
+def minHeightBst(array):
+    return minHeightBstHelper(array, 0, len(array) - 1)
+
+def minHeightBstHelper(array, left, right):
+	if left > right:
+		return None
+	mid = (left + right) // 2
+	val = array[mid]
+	tree = BST(val)
+	tree.left = minHeightBstHelper(array, left, mid-1)
+	tree.right = minHeightBstHelper(array, mid+1, right)
+	return tree
+	
+class BST:
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
