a281dc26bd2217c62af4b5cd25ebda2e82b27785
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-12 20:06:20 -0800

    Add files via upload

---
+++ MonotonicArray/MonotonicArray.py
@@ -0,0 +1,11 @@
+# Time = O(n)
+# Space = O(1)
+def isMonotonic(array):
+    increase = True
+	decrease = True
+	for idx in range(len(array)-1):
+		if array[idx] > array[idx+1]:
+			increase = False
+		if array[idx] < array[idx+1]:
+			decrease = False
+	return increase or decrease 
