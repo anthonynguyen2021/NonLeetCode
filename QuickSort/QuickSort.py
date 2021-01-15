b551c268b5029481442a63f7a97bb25f95788081
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-14 19:25:26 -0800

    Create QuickSort.py

---
+++ QuickSort/QuickSort.py
@@ -0,0 +1,27 @@
+# Time = O(nlogn)
+# Space = O(logn) - recursive calls
+def quickSort(array):
+    recurseQS(array, 0, len(array) - 1)
+	return array
+
+def recurseQS(array, left, right):
+	if left < right:
+		mid = partition(array, left, right)
+		recurseQS(array, left, mid-1)
+		recurseQS(array, mid+1, right)
+	return None
+
+def partition(array, left, right):
+	pivotVal = array[left]
+	first = left + 1
+	last = right
+	while True:
+		while first <= last and array[first] <= pivotVal:
+			first += 1
+		while first <= last and array[last] >= pivotVal:
+			last -= 1
+		if first > last:
+			break
+		array[first], array[last] = array[last], array[first]
+	array[left], array[last] = array[last], array[left]
+	return last
