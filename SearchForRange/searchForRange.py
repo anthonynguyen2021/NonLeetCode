b61feb9186e678138eb6c387c377ea6f0e503cfd
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-18 18:14:29 -0800

    Create searchForRange.py

---
+++ SearchForRange/searchForRange.py
@@ -0,0 +1,25 @@
+# Time = O(log n)
+# Space = O(1)
+def searchForRange(array, target):
+	goLeft = True
+    left = helperFindRange(array, target, goLeft)
+	right = helperFindRange(array, target, False)
+	return [left, right]
+
+def helperFindRange(array, target, goLeft):
+	firstIdx, lastIdx = -1, -1
+	left, right = 0, len(array) - 1
+	while left <= right:
+		mid = (left + right) // 2
+		if array[mid] == target:
+			if goLeft:
+				firstIdx = mid
+				right = mid-1
+			else:
+				lastIdx = mid
+				left = mid+1
+		elif array[mid] > target:
+			right = mid-1
+		else:
+			left = mid+1
+	return firstIdx if goLeft else lastIdx 
