2ef1af95137c80d5191746dd1736d3d72446e087
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-19 19:29:35 -0800

    Create findSuccessorRecursive.py

---
+++ FindSuccessor/findSuccessorRecursive.py
@@ -0,0 +1,35 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+# Idea of solution: Write a recursive method to find successor. If we know that we're going downward, we can write a recursive method starting with the node's right child.
+# Otherwise, start a recursive method in finding the parent and the correct next order.
+
+# Time = O(h)
+# Space = O(h)
+def findSuccessor(tree, node):
+    goDown = False
+	if node.right:
+		goDown = True
+		return recursiveSuccessor(tree, node.right, goDown)
+	else:
+		return recursiveSuccessor(tree, node, goDown)
+	
+def recursiveSuccessor(tree, node, goDown):
+	if goDown:
+		if not node.left:
+			return node
+		else:
+			return recursiveSuccessor(tree, node.left, goDown)
+	else:
+		if not node.parent:
+			return None
+		elif node.parent.left == node:
+			return node.parent 
+		else:
+			return recursiveSuccessor(tree, node.parent, goDown)
+		
