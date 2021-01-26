788d2c1c74ac76f7861e5c4776d296ddb30d491b
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-26 13:56:10 -0800

    Update shiftedLinkedList.py

--- shiftLinkedList/shiftedLinkedList.py
+++ shiftLinkedList/shiftedLinkedList.py
@@ -1,7 +1,11 @@
 # Time = O(n) - find the length of the list and traverse it again to get to the new tail. At most 2n.
 # Space = O(1) - Using constant number of pointers
 
-# Idea of solution: 
+# Idea of solution: If we had the pointer to the head, tail, new tail, we can get the shifted linked list. The new head is just newTail.next. The oldTail connects to the old head.
+# To get to the tail, you need to traverse the list. It's a good idea to keep track of the length of the list since k is an integer. We may need to make sure that
+# k is in the interval (-l, l) where l is the length of the list. Using this, we need to figure out how many steps from the head we need to take to get to the new tail. 
+# We note that if k < 0, then we take -k-1 steps. If k > 0, we take l-k-1 steps. Try this on our test case to see. 
+# See line 17-23 to see this idea in action. 
 def shiftLinkedList(head, k):
     # Find length of linkedlist
 	length = 1
