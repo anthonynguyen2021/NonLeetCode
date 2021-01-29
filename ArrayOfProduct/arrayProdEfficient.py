ce01a3637db750c55ad0ffc9e27e38dbdd9b55e5
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-01-28 21:32:13 -0800

    Update arrayProdEfficient.py

--- ArrayOfProduct/arrayProdEfficient.py
+++ ArrayOfProduct/arrayProdEfficient.py
@@ -3,6 +3,9 @@
 # Idea is to get a running product to the left and right of index idx. We initialize an array with 1s.
 # We populate the left running product first with a for loop. Next, we keep track of a right running product
 # from index len(array)-1, ... , 0 and multiply it to prod[idx].
+
+# Thinking: If we two arrays of size n where we keep track of the running products to the left of each index and to the right of each index, we're almost done. The solution is the
+# hadamard product of these two arrays. To do better, 1 auxillary array is needed. Form the left running product and think how you can do it without any additional expensive storage.
 def arrayOfProducts(array):
 	if len(array) == 1:
 		return array
