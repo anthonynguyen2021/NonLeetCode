bb5af73588fd5d10d39f22636457097ef546a121
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-02 00:18:07 -0800

    Create LongestSubstringEfficient.py

---
+++ LongestSubstringWithoutDuplicates/LongestSubstringEfficient.py
@@ -0,0 +1,26 @@
+# Time = O(N)
+# Space = O(min(A, N)) where A is the length of the # of distinct characters in string
+
+# The logic can be derivted as follows:
+
+# 1. By induction, at index i, ask if you've seen character string[i] before index i. If not, do nothing.
+# If so, move the startIdx to max(startIdx, seen[char]+1) - this means either the seen[char]'s index is 
+# to the right of startIdx or seen[char]'s index is at most startIdx. Note: startIdx is the start of the 
+# substring you're considering.
+# Next, we check the length of the current substring in question and update longest substring; we 
+# update the index of the last time we've seen chara, which is i; the order of these two don't matter
+
+# 2. Next, check if the length of the substring string[startIdx: i+1] is the largest you've seen. If so
+# record it. 
+# 3. Update seen[char]'s index to index i. This says that you've last seen char at index i. 
+def longestSubstringWithoutDuplication(string):
+	startIdx = 0
+	seen = dict()
+	longest = [0, 1]
+    for i, char in enumerate(string):
+		if char in seen:
+			startIdx = max(startIdx, seen[char]+1)
+		seen[char] = i  # The order of line 19 and 20 doesn't matter
+		if i - startIdx + 1 > longest[1] - longest[0]:
+			longest = [startIdx, i+1]
+	return string[longest[0]:longest[1]]
