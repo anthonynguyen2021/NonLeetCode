daf7e3ae10fe3ff038d2492840595c2152aae947
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-06 00:55:51 -0800

    Update minWait.py

--- minimumWaitTime/minWait.py
+++ minimumWaitTime/minWait.py
@@ -1,3 +1,6 @@
+# Since the ith task depends on the sum of the previous i-1 tasks, it makes sense these i-1 tasks are the smallest. In other words, the smallest i-1 elements are in the first
+# i-1 slots. So sort the array in place which is O(nlogn). We can do this without extra space by storing all the previous cumulative tasks time and add it to the current time (see code below).
+
 # Time = O(nlogn)
 # Space = O(1)
 def minimumWaitingTime(queries):
