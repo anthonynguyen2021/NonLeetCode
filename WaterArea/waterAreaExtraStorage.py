3fa39c3087a37041d4a4c53238f780e18c80879f
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-08 23:27:22 -0800

    Update waterAreaExtraStorage.py

--- WaterArea/waterAreaExtraStorage.py
+++ WaterArea/waterAreaExtraStorage.py
@@ -1,6 +1,7 @@
 # Idea of Solution: Use an auxilliary space to keep track of largest seen to the left / right of index i - use two arrays of size n to keep track of this. 
 # If the minimum of the largest seen to the left / right of index i is larger than heights[i], it means there are water trapped at index i with area 
 # min(largestSeenLeft[i], largestSeenRight[i]) - heights[i]
+# To optimize this approach, you can just use extra 1 array of size n and do some overwriting in this array.
 
 # Time = O(n)
 # Space = O(n)
