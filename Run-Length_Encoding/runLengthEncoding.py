97d33e85f796b9639e67d73484c521e9b72bcfdf
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-09 20:20:40 -0800

    Update runLengthEncoding.py

--- Run-Length_Encoding/runLengthEncoding.py
+++ Run-Length_Encoding/runLengthEncoding.py
@@ -1,4 +1,6 @@
-# Explanation of solution: 
+# Explanation of solution: The idea is to keep track of the previous character seen and count except at index i as you do a for loop through the length of the string.
+# At the current character, if you encounter a different one from previous character, append the encoding to a list and update the previous character. If the previous counter 
+# is 9, add that to the list and set the counter to 0.
 
 # Time = O(n)
 # Space = O(n) to store output
