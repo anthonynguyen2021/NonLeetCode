f26b5bcbacd0b14965f7fcda637ad314e0c57580
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-10 23:11:33 -0800

    Create KadanesBruteForce.py

---
+++ KadanesAlgorithm/KadanesBruteForce.py
@@ -0,0 +1,13 @@
+# Time = O(n^3)
+# Space = O(1)
+
+# Brute Force Solution: Note that the nested loop automatically gives us O(n^2). Summing is on average O(n/2) for subarray of size n. Therefore, the complexity is O(n^3).
+
+
+def kadanesAlgorithm(array):
+    largestSum = float('-inf')
+	for i in range(len(array)):
+		for j in range(i, len(array)):
+			currentSum = sum(array[i:j+1])
+			largestSum = currentSum if currentSum > largestSum else largestSum
+	return largestSum
