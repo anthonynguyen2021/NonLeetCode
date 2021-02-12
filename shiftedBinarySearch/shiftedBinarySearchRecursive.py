58fae2201eeaaab91441889e224cae21a12001aa
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-12 00:54:58 -0800

    Create shiftedBinarySearchRecursive.py

---
+++ shiftedBinarySearch/shiftedBinarySearchRecursive.py
@@ -0,0 +1,26 @@
+# Time = O(logn)
+# Space = O(logn)
+
+# Idea of solution (recursive): Similar to the iterative approach except at each case where we update
+# the pointers, we make a recursive call of the function. 
+
+def shiftedBinarySearch(array, target):
+    return helperShiftedBinarySearch(array, target, 0, len(array)-1)
+
+def helperShiftedBinarySearch(array, target, left, right):
+	if left > right:
+		return -1
+	middle = (left + right) // 2
+	if target == array[middle]:
+		return middle
+	if array[left] < array[middle]:
+		if array[left] <= target < array[middle]:
+			return helperShiftedBinarySearch(array, target, left, middle-1)
+		else:
+			return helperShiftedBinarySearch(array, target, middle+1, right)
+	else:
+		if array[middle] < target <= array[right]:
+			return helperShiftedBinarySearch(array, target, middle+1, right)
+		else:
+			return helperShiftedBinarySearch(array, target, left, middle-1)
+	
