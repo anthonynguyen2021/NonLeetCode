804b5ed5525b7f5a310b146d56243fa21417b528
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-14 02:23:58 -0800

    Create diameterOfTwoArr.py

---
+++ smallestDifference/diameterOfTwoArr.py
@@ -0,0 +1,23 @@
+# Idea of Solution: 
+
+# Time = O(nlogn + mlogm) where n, m are lengths of array1, array2 respectively.
+# Space = O(1)
+
+def smallestDifference(arrayOne, arrayTwo):
+  arrayOne.sort()
+	arrayTwo.sort()
+	oneIdx = twoIdx = 0
+	smallestDistance = float('inf')
+	while oneIdx < len(arrayOne) and twoIdx < len(arrayTwo):
+		distance = abs(arrayOne[oneIdx]-arrayTwo[twoIdx])
+		if distance < smallestDistance:
+			smallestDistance = distance
+			smallOne = arrayOne[oneIdx]
+			smallTwo = arrayTwo[twoIdx]
+		if arrayOne[oneIdx] < arrayTwo[twoIdx]:
+			oneIdx += 1
+		elif arrayOne[oneIdx] > arrayTwo[twoIdx]:
+			twoIdx += 1
+		else:
+			return [smallOne, smallTwo]
+	return [smallOne, smallTwo]
