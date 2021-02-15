a1e2e1591258839ce439d8f336da64ef42ba283f
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-15 02:52:56 -0800

    Create invertBinaryTree.py

---
+++ invertBinaryTree/invertBinaryTree.py
@@ -0,0 +1,21 @@
+# Idea of solution: You can equivalently think if this problem as asking write a function
+# that takes in a binary tree so that for each of the node, the left and right children are swap.
+
+# Time = O(n)
+# Space = O(h) where h is the height of the tree
+
+def invertBinaryTree(tree):
+    if not tree:
+		return tree
+	tree.left, tree.right = tree.right, tree.left
+	invertBinaryTree(tree.left)
+	invertBinaryTree(tree.right)
+	return tree
+
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None
