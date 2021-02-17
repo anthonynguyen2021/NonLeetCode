e7f3520fd54c641236aa810275735a027e8e784c
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-16 23:44:17 -0800

    Create nonconstructibleChange.py

---
+++ Non-constructibleChange/nonconstructibleChange.py
@@ -0,0 +1,12 @@
+# Time = O(nlogn) where n is the length of the input array and we used an in place sorting algorithm
+# Space = O(1)
+
+def nonConstructibleChange(coins):
+	coins.sort()
+	change = 0
+	for idx in range(len(coins)):
+		value = coins[idx]
+		if value > change+1:
+			return change+1
+		change += value
+	return change+1
