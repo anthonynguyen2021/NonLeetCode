d6f44e2d517ec72006ec4cb48b9ae53ee7cc42bb
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-17 23:34:17 -0800

    Update classPhoto.py

--- ClassPhoto/classPhoto.py
+++ ClassPhoto/classPhoto.py
@@ -1,3 +1,12 @@
+# Idea of solution: We sort both arrays. With no loss in generality, we may assume the back row are blue shirt students. If such a solution exists, we can find such 
+# photo arrangements. Suppose we sorted our two input arrays. We will prove that it sufficeds to show that b_i > r_i for all i. Let redShirtHeights = [r_1, ...., r_n] 
+# & blueShirtHeights = [b_1, ..., b_n]. By existence of solution, we may assume there exists a permutation (bijective) function f, so that r_f(i) < b_f(i).
+# Note that b_n > a_i for some i. Also, note that for some k, we have b_k > a_n. Since these input arrays are sorted, we can rearrange these 4 students so that b_n > a_n 
+# due to b_k > a_n and b_k > a_i since b_k > a_n. By induction, the results hold. 
+
+# The pairings in the above explanation can be viewed as a bipartite graph where exactly 1 node from first set connects to exactly 1 node in the second set; in other words,
+# each vertex has degree 1.
+
 # Time = O(nlogn) 
 # Space = O(1)
 
