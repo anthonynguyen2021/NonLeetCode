88377d0756740f806d982f917aceac85eab91e06
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-17 23:35:23 -0800

    Create bfs.py

---
+++ breath-first-search/bfs.py
@@ -0,0 +1,23 @@
+# Do not edit the class below except
+# for the breadthFirstSearch method.
+# Feel free to add new properties
+# and methods to the class.
+class Node:
+    def __init__(self, name):
+        self.children = []
+        self.name = name
+
+    def addChild(self, name):
+        self.children.append(Node(name))
+        return self
+# Time = O(v + e)
+# Space = O(e) 
+    def breadthFirstSearch(self, array):
+        queue = []
+		queue.append(self)
+		while queue:
+			currentNode = queue.pop(0)
+			array.append(currentNode.name)
+			for neighbor in currentNode.children:
+				queue.append(neighbor)
+		return array 
