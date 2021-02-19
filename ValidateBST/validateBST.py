721011c2f216b8c75d8f942cac3922098ac6ff91
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-19 02:43:34 -0800

    Update validateBST.py

--- ValidateBST/validateBST.py
+++ ValidateBST/validateBST.py
@@ -6,8 +6,8 @@ class BST:
         self.right = None
 	
 # Idea of solution: The definition of BST is a binary tree where each node has the BST property. This can be done by recursion. The base case is when the node is empty.
-# For general tree node, if that node is a left child, you check if its value is strictly less than its parent's value (upper bound); the lower bound is the parent's value if the node is a right child or its nearest ancestor where its a right child.
-# If the node is a right child, you check if its value is at least as great as its parent's value (lower bound); for the upper bound, find the nearest ancestor that is a left child (use  that value). To start off the root,
+# For general tree node, if that node is a left child, you check if its value is strictly less than its parent's value (upper bound); the lower bound is its nearest ancestor where its a right child which you use that ancestor's parental value.
+# If the node is a right child, you check if its value is at least as great as its parent's value (lower bound); for the upper bound, find the nearest ancestor that is a left child (use its parental value). To start off the root,
 # set lower bound to be -infinity and upperbound to be infinity.
 
 # Time = O(n) where n is the number of nodes in the tree
