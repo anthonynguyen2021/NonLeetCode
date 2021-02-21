92876c4b3a03ec63852b6c86ae33ae37326c121b
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-02-21 04:09:56 -0800

    Update buildMinHeap.py

--- MinHeapConstruction/buildMinHeap.py
+++ MinHeapConstruction/buildMinHeap.py
@@ -13,59 +13,59 @@ class MinHeap:
 	# Time = O(n)
 	# Space = O(1)
     def buildHeap(self, array):
-		if len(array) < 2:
-			return array
-        lastIdx = (len(array) - 2) // 2
-		for i in reversed(range(0, lastIdx+1)):
-			self.siftDown(array, i, len(array))
+	if len(array) < 2:
 		return array
+	lastIdx = (len(array) - 2) // 2
+	for i in reversed(range(0, lastIdx+1)):
+		self.siftDown(array, i, len(array))
+	return array
 	# Time = O(log n) - depending on what level you're at in the heap.
 	# Space = O(1) using the iterative approach.
     def siftDown(self, array, index, endIdx):
-		while True:
-			left, right = 2 * index + 1, 2 * index + 2
-			smallest = index
-			if left < endIdx and array[smallest] > array[left]:
-				smallest = left
-			if right < endIdx and array[smallest] > array[right]:
-				smallest = right
-			if smallest != index:
-				array[index], array[smallest] = array[smallest], array[index]
-				index = smallest
-			else:
-				break
+	while True:
+		left, right = 2 * index + 1, 2 * index + 2
+		smallest = index
+		if left < endIdx and array[smallest] > array[left]:
+			smallest = left
+		if right < endIdx and array[smallest] > array[right]:
+			smallest = right
+		if smallest != index:
+			array[index], array[smallest] = array[smallest], array[index]
+			index = smallest
+		else:
+			break
 
 	# Time = O(log n)
 	# Space = O(1)
     def siftUp(self, array, index):
-		current = index
-    	parent = (current - 1) // 2  # Parent node is given by (index - 1) // 2
-		while parent >= 0:
-			if array[current] < array[parent]:
-				array[current], array[parent] = array[parent], array[current]
-				current = parent
-				parent = (current - 1) // 2
-			else:
-				break
+	current = index
+	parent = (current - 1) // 2  # Parent node is given by (index - 1) // 2
+	while parent >= 0:
+		if array[current] < array[parent]:
+			array[current], array[parent] = array[parent], array[current]
+			current = parent
+			parent = (current - 1) // 2
+		else:
+			break
 	# If the min heap is not empty, we can peak at it.
 	
 	# Time = O(1)
 	# Space = O(1)
     def peek(self):
         if self.heap:
-			return self.heap[0]
+		return self.heap[0]
 
 	# Time = O(log n) - Swap the minimum element with the last element in the array. Pop out the elemnt.
 	# Sift down starting at index 0.
 	# Space = O(1) 
     def remove(self):
         self.heap[0], self.heap[-1] = self.heap[-1], self.heap[0]
-		pop = self.heap.pop()
-		self.siftDown(self.heap, 0, len(self.heap))
-		return pop
+	pop = self.heap.pop()
+	self.siftDown(self.heap, 0, len(self.heap))
+	return pop
 	
 	# Time = O(logn) - uses the sift up which is O(log n) in time
 	# Space = O(1)
     def insert(self, value):
         self.heap.append(value)
-		self.siftUp(self.heap, len(self.heap)-1)
+	self.siftUp(self.heap, len(self.heap)-1)
