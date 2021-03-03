414c15f645bc6bcaba7fe8983609b176ec8b5eae
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-02 22:01:03 -0800

    Create generateDocument.py

---
+++ GenerateDocument/generateDocument.py
@@ -0,0 +1,16 @@
+# Time = O(m+n) where m is the length of characters and n is the length of document
+# Space = O(m+n)
+def generateDocument(characters, document):
+    hashMapFrequency = {}
+	for char in characters:
+		if char in hashMapFrequency:
+			hashMapFrequency[char] += 1
+		else:
+			hashMapFrequency[char] = 1
+	for char in document:
+		if char not in hashMapFrequency:
+			return False
+		hashMapFrequency[char] -= 1
+		if hashMapFrequency[char] < 0:
+			return False
+	return True
