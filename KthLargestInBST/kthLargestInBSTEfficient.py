3bd7025c73bf9a12dd620aeaa086894096b7502a
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-03 21:22:15 -0800

    Update kthLargestInBSTEfficient.py

--- KthLargestInBST/kthLargestInBSTEfficient.py
+++ KthLargestInBST/kthLargestInBSTEfficient.py
@@ -10,8 +10,15 @@ class TreeInfo:
 		self.visit = visit
 		self.value = value
 
+# Idea of Solution: Note that if we did a reverse in-order traversal, we get a list of nodal values from largest to smallest. By reverse in-order traversal, we mean
+# at a current node, visit the right subtree, visit the current node, and visit the left subtree recursively. We need TreeInfo class to store these global variables that 
+# track the number of visited nodes and the current value. The idea is to code the reverse in-order traversal (similar to the in-order traversal) and before we visit a node, 
+# check if it's at the kth node. If not, increment the visit by 1 and store the current nodal value in value.  
+		
+# Time = O(h + k) - worst case to get to the largest value is O(h) where h is the depth of the tree. From there, we have k more nodes to visit.
+# Space = O(h) due to the call stack from recursion.
 def findKthLargestValueInBst(tree, k):
-    treeData = TreeInfo(0, None)
+    	treeData = TreeInfo(0, None)
 	reverseInOrder(tree, k, treeData)
 	return treeData.value
 
