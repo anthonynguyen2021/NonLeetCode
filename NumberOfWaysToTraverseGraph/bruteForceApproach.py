492b4b3058e1d20c39a466afe13176e9cd14d59c
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-04 21:14:30 -0800

    Update bruteForceApproach.py

--- NumberOfWaysToTraverseGraph/bruteForceApproach.py
+++ NumberOfWaysToTraverseGraph/bruteForceApproach.py
@@ -1,3 +1,5 @@
+# Idea of Approach is via recursion. Note at grid (i, j), to compute this, we need to figure out the number of ways to get to (i-1, j) and (i, j-1) and add them.
+
 def numberOfWaysToTraverseGraph(width, height):
     	if width == 1 or height == 1:
 		return 1
