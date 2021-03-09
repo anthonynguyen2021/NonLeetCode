039f713b2e7219e4f030be739bc9ea2098e10372
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-09 00:19:08 -0800

    Update sudoku.py

--- SolveSudoku/sudoku.py
+++ SolveSudoku/sudoku.py
@@ -1,4 +1,9 @@
-# Idea Solution: 
+# Idea Solution: The idea is to use backtracking. We solve the sudoku board from row1 from left to right. Then go to row 2, from left to right, and so on. At each 0's we see, 
+# We put a digit {1, 2, ... , 9} in that position (make sure it's valid) and call the function on the next spot. This function returns a bool meaning if we can solve it
+# at the current row and column. 
+# To understand the recursive nature of the solution, assume we're at (row, col) and we have solved the board correctly so far. Suppose at board[row][col], we have 0. 
+# If we place a 1 there, we call the recursive function at index row, col+1. If we can't solve it, return False. Otherwise, return True. However if we can't solve it at
+# (row, col) with 1 there, we check 2 and call the recursive function at index row, col+1 again. 
 
 # Time = O(1) | Space = O(1) - the board is always a 9x9 so the amount of computation is always the same. The only additional storage is through the call stack, which is always
 # constant. 
