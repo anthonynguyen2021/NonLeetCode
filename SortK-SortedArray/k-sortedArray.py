a09a8e93caab632b45283417f96df6f1b6a02711
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-13 02:17:12 -0800

    Update k-sortedArray.py

--- SortK-SortedArray/k-sortedArray.py
+++ SortK-SortedArray/k-sortedArray.py
@@ -3,7 +3,7 @@ import heapq
 # Time = O(k + nlogk)
 # Space = O(k)
 def sortKSortedArray(array, k):
-    	firstK = array[:min(k+1, len(array))]
+	firstK = array[:min(k+1, len(array))]
 	heapq.heapify(firstK)
 	idxToInsert = 0
 	for idx in range(k+1, len(array)):
@@ -15,4 +15,4 @@ def sortKSortedArray(array, k):
 		minElement = heapq.heappop(firstK)
 		array[idxToInsert] = minElement
 		idxToInsert += 1
-    return array
+    	return array
