e0a566400d3381313464d23dd78b5960871e0dcf
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-13 02:16:42 -0800

    Create k-sortedArray.py

---
+++ SortK-SortedArray/k-sortedArray.py
@@ -0,0 +1,18 @@
+import heapq
+
+# Time = O(k + nlogk)
+# Space = O(k)
+def sortKSortedArray(array, k):
+    firstK = array[:min(k+1, len(array))]
+	heapq.heapify(firstK)
+	idxToInsert = 0
+	for idx in range(k+1, len(array)):
+		minElement = heapq.heappop(firstK)
+		array[idxToInsert] = minElement
+		idxToInsert += 1
+		heapq.heappush(firstK, array[idx])
+	while len(firstK) != 0:
+		minElement = heapq.heappop(firstK)
+		array[idxToInsert] = minElement
+		idxToInsert += 1
+    return array
