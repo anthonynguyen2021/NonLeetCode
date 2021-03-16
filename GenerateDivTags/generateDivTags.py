8960763a927f836f7aeaa6a4df151b5990ba84a5
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-16 00:04:32 -0700

    Update generateDivTags.py

--- GenerateDivTags/generateDivTags.py
+++ GenerateDivTags/generateDivTags.py
@@ -1,8 +1,12 @@
+# Explanation of solution: One can prove the catalin numbers C_n := (2n)! / (n! * n! * (n+1)) is equal to the number of such
+# valid brackets in this problem using n symbols of '[' and ']'. For the space complexity, we have that '[' and ']' corresponds to
+# '<div>' and '</div>', respectively. But they consists of 11 characters for each valid enclosure. So S(n) = T(n) * 11n = T(n) * n.
+
 # Time = O((2n)! / n!^2 * (n+1)) | Space = T(n) * 11n
 def generateDivTags(numberOfTags):
-    listMatchedTags = []
+    	listMatchedTags = []
 	buildValidMatchedTags(numberOfTags, numberOfTags, "", listMatchedTags)
-    return listMatchedTags
+    	return listMatchedTags
 
 def buildValidMatchedTags(left, right, currentString, result):
 	if left > 0:
