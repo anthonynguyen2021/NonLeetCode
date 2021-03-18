c3bac636d818a80dea70f601f213fd0282786ba7
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-17 22:53:48 -0700

    Update startingCity.py

--- validStartingCity/startingCity.py
+++ validStartingCity/startingCity.py
@@ -1,3 +1,5 @@
+# Idea of solution: Start at each city and check if we can make the trip. For each starting point, we do O(n) work, so n*n = n^2.
+
 # Time = O(n^2) | Space = O(1)
 def validStartingCity(distances, fuel, mpg):
 	for i in range(len(distances)):
