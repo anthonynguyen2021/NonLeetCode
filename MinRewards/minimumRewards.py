83f995d52092fdb545fdcd3aa71502b55d1da186
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-17 20:11:00 -0700

    Update minimumRewards.py

--- MinRewards/minimumRewards.py
+++ MinRewards/minimumRewards.py
@@ -1,3 +1,11 @@
+# Idea of solutions #1-3: At index i, if scores[i-1] < scores[i], you want rewards[i+1] = rewards[i] + 1 - draw a couple of examples. Suppose at index i, you have 
+# scores[i] > scores[i+1]; either rewards[i] = rewards[i+1]+1 or rewards[i] = rewards[i] since the left side might be larger - draw a picture of ann increasing line from the left
+# and compare it from a decreasing line from index i to the right.
+
+# Idea of Solution 1: Note that a local min has reward 1 to be optimal. Initialize rewards = [1 for _ in scores] Go from index 1 to the end. If scores[i] > scores[i-1], 
+# set rewards[i] = rewards[i-1] + 1. If scores[i] < scores[i-1], you set rewards[i-1] = max(rewards[i-1], rewards[i]+1). Now do this for index i-1: that is, if scores[i-2] > scores[i-1],
+# do the same step again. 
+
 # Time = O(n^2) | Space = O(n)
 def minRewards(scores):
 	rewards = [1 for _ in scores]
