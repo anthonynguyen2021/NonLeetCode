d910de189edc5e5382f659580b9e002099a346ea
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:37:52 -0700

    Update IndexEqualsValue.py

--- IndexEqualsValue/IndexEqualsValue.py
+++ IndexEqualsValue/IndexEqualsValue.py
@@ -1,6 +1,9 @@
+# Idea of solution: Note that if array[mid] = mid, we need to check the left of mid to see if we get a fixed point. If array[mid] > mid, by sortedness of the array, we don't
+# check array[j] where j > mid. So right = mid - 1. Otherwise, set left = mid+1 by similar argument.
+
+# Time = O(log n)
+# Space = O(1)
 def indexEqualsValue(array):
-	# Time = O(log n)
-	# Space = O(1)
 	index = -1
 	left = 0
 	right = len(array) - 1
