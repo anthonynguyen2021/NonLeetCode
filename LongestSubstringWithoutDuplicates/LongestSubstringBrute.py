601d05529234769f2f0d5012cc3076cdbbef2a95
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:47:34 -0700

    Update LongestSubstringBrute.py

--- LongestSubstringWithoutDuplicates/LongestSubstringBrute.py
+++ LongestSubstringWithoutDuplicates/LongestSubstringBrute.py
@@ -1,11 +1,11 @@
 # Brute force approach
-# At each index of the string, we keep marching until we seee a duplicate. We keep track of the length of this substring and store how to get this substring somewhere.
+# Solution Idea: At each index of the string, we keep marching until we seee a duplicate. We keep track of the length of this substring and store how to get this substring somewhere.
 # Do this from index 0 to len(string) - 1 while keeping track of the largest substring seen so far. Then we return the largest substring.
 
 # Time = O(n^2)
 # Space = O(n)
 def longestSubstringWithoutDuplication(string):
-    largest = 0
+    	largest = 0
 	substring = dict()
 	for index in range(len(string)):
 		left, right, sizeSubstring = helperSubstring(string, index)
