ff933fbd912a5d7239a2ac085b1e6b7b0d1e2f6e
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 02:20:18 -0700

    Update invertBinaryTree.py

--- invertBinaryTree/invertBinaryTree.py
+++ invertBinaryTree/invertBinaryTree.py
@@ -15,7 +15,7 @@ def invertBinaryTree(tree):
 
 # This is the class of the input binary tree.
 class BinaryTree:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
+	def __init__(self, value):
+		self.value = value
+		self.left = None
+		self.right = None
