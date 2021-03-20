8225d4dc86722c9761639d1944996c12528c00ef
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:45:56 -0700

    Update longestPalindromeSubstring.py

--- LongestPalindromeSubstring/longestPalindromeSubstring.py
+++ LongestPalindromeSubstring/longestPalindromeSubstring.py
@@ -1,9 +1,11 @@
+# Idea of solution: For each substring string[i:j+1], check if it's a palindrome and the longest seen so far.
+
 # Time = O(n^3) - O(n^2) for each potential substring and O(n) for palindrome
 # Space = O(n) - to slice the string at the end
 def longestPalindromicSubstring(string):
 	if not string:
 		return ''
-    longest = float('-inf')
+    	longest = float('-inf')
 	for i in range(len(string)):
 		for j in range(i, len(string)):
 			if palindrome(string, i, j) and j-i+1 > longest:
