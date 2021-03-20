980a399ca92c2ed4c8224d6c38b58dacf658a252
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:58:13 -0700

    Update numberTopologies.py

--- NumberOfBinaryTreesTopology/numberTopologies.py
+++ NumberOfBinaryTreesTopology/numberTopologies.py
@@ -1,9 +1,9 @@
 # Time = O(2n choose n)
 # Space = O(n) due to the dictionary and the recursive function calls on the call stack
 
-# Idea: By memoization. Solution by recursion.
+# Idea: By memoization. Solution by recursion. A tree with n+1 node has left subtree of size i and right subtree of size n-i where i is in {0, 1, ..., n} and use multiplication rule.
 def numberOfBinaryTreeTopologies(n):
-    return helperNumTopology(n)
+	return helperNumTopology(n)
 
 def helperNumTopology(n, memoize={0:1, 1:1, 2:2}):
 	if n in memoize:
