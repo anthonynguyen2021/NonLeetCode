57ea8c3f0a63024fda13fc4a0ea87b7e5c7adc03
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:40:58 -0700

    Update kthLargestInBST.py

--- KthLargestInBST/kthLargestInBST.py
+++ KthLargestInBST/kthLargestInBST.py
@@ -1,10 +1,10 @@
 # This is an input class. Do not edit.
 class BST:
-    def __init__(self, value, left=None, right=None):
-        self.value = value
-        self.left = left
-        self.right = right
-	
+	def __init__(self, value, left=None, right=None):
+		self.value = value
+		self.left = left
+		self.right = right
+
 # Idea of solution: When we traverse a BST in the in-order traversal way, the resulting visited nodes are sorted. We store the list of visited values and look at 
 # the len(array)-k index for the kth largest. 
 
