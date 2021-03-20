f9b85f9c8e7814ca6db2b89866b8b7835588d343
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:44:39 -0700

    Update longestCommonSubsequenceBruteForce.py

--- LongestCommonSubsequence/longestCommonSubsequenceBruteForce.py
+++ LongestCommonSubsequence/longestCommonSubsequenceBruteForce.py
@@ -10,7 +10,7 @@
 # a) str1[0:len(str1)-1] and str2 or b) str1 and str2[0:len(str2)-1]
 
 def longestCommonSubsequence(str1, str2):
-    longestDP = [[[] for _ in range(len(str2)+1)] for _ in range(len(str1)+1)]
+    	longestDP = [[[] for _ in range(len(str2)+1)] for _ in range(len(str1)+1)]
 	for i in range(1, len(str1)+1):
 		for j in range(1, len(str2)+1):
 			if str1[i-1] == str2[j-1]:
