f3e04afa3d2664778f8c9da2aea48866d68f65ba
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:31:25 -0700

    Update findSuccessorSpace.py

--- FindSuccessor/findSuccessorSpace.py
+++ FindSuccessor/findSuccessorSpace.py
@@ -1,10 +1,10 @@
 # This is an input class. Do not edit.
 class BinaryTree:
-    def __init__(self, value, left=None, right=None, parent=None):
-        self.value = value
-        self.left = left
-        self.right = right
-        self.parent = parent
+	def __init__(self, value, left=None, right=None, parent=None):
+		self.value = value
+		self.left = left
+		self.right = right
+		self.parent = parent
 	
 # Idea: Use extra space to store the in-order traversal of the tree. Go through the list and if we see node, return the next in the list. We append None at the end to avoid
 # an edge case and makes our code easier to read. 
@@ -13,7 +13,7 @@ class BinaryTree:
 # Time = O(n)
 # Space = O(n)
 def findSuccessor(tree, node):
-    inOrderTraversal = helperInOrder(tree)
+	inOrderTraversal = helperInOrder(tree)
 	inOrderTraversal.append(None)
 	for idx, vertex in enumerate(inOrderTraversal):
 		if vertex == node:
