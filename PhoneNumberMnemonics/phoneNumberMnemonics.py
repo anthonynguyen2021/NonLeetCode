a25e2a6e386f054dd665419b93bc52cb911bddb2
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:59:18 -0700

    Update phoneNumberMnemonics.py

--- PhoneNumberMnemonics/phoneNumberMnemonics.py
+++ PhoneNumberMnemonics/phoneNumberMnemonics.py
@@ -5,7 +5,7 @@
 # Time = O(n * 4^n) - 4^n because we visited all 4^n possible strings and appending string of length n so the copying takes that long
 # Space = O(n * 4^n) - 4^n is all possible strings in the worst case with length n
 def phoneNumberMnemonics(phoneNumber):
-    phoneBook = {'1': '1', '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz', '0': '0'}
+    	phoneBook = {'1': '1', '2': 'abc', '3': 'def', '4': 'ghi', '5': 'jkl', '6': 'mno', '7': 'pqrs', '8': 'tuv', '9': 'wxyz', '0': '0'}
 	result = []
 	buildMnemonics(phoneNumber, phoneBook, result, 0, "")
 	return result
