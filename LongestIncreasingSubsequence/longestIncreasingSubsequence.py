5388edf43b0bce21d2f1bd8d3b864fe2e4fd1adc
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-20 01:07:53 -0700

    Update longestIncreasingSubsequence.py

--- LongestIncreasingSubsequence/longestIncreasingSubsequence.py
+++ LongestIncreasingSubsequence/longestIncreasingSubsequence.py
@@ -1,4 +1,10 @@
+# Explanation of solution: We use DP to build our solution. The longestSubsequence array is the solution we build where index i is the largest length of subsequence of array[:i+1]
+# where index i is included. For index i, we check index j in {0, ... , i-1} to see if we have largest subsequence from array[:j+1] including index j plus index i exceeds what
+# is currently stored at longestSubsequence[i]. We keep track of the index in longestSubsequence that corresponds to the largest sized subsequence in array and use previoudIdx
+# array to backtrack and build our subsequence where that array notes the previous part of the subsequence.
+
 # Time = O(n^2) | Space = O(n)
+# Explanation: Time comes from the two for loop, and space comes from longestSubsequennce & previousIdx array. Note that buildSequence method is O(n) time and space. 
 def longestIncreasingSubsequence(array):
     	longestSubsequence = [1 for _ in array]
 	previousIdx = [None for _ in array]
