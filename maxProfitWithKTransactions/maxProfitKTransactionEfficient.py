713a5dfba98f8360abffcde222a37e41014b7ca7
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-22 01:36:47 -0700

    Update maxProfitKTransactionEfficient.py

--- maxProfitWithKTransactions/maxProfitKTransactionEfficient.py
+++ maxProfitWithKTransactions/maxProfitKTransactionEfficient.py
@@ -1,3 +1,9 @@
+# Explanation of pointers: Note that when row == 1, currentRow & oddRowDP are used interchangable as they point to the same thing and previousRow & evenRowDP points to the same
+# thing interchangably. When row == 2, previousRow, oddRow point to the same thing by how Python assigns previousRow = oddRowDP and currentRow, evenRowDP point to the same object.
+# Reread this if this solution seems strange.
+
+# Explanation of solution: In the previous solution, note that we only used the previous and current row of the DP build up. So two rows are only needed. 
+
 # Time = O(kn) | Space = O(n) where k and n are # of transactions and n = length of prices, respectively.
 def maxProfitWithKTransactions(prices, k):
 	if not len(prices):
