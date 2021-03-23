a8a832741ffc8f5fe16f48825b35734c4e2fbb38
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-23 00:02:31 -0700

    Create KNP.py

---
+++ Knuth-Morris-Pratt-Algorithm/KNP.py
@@ -0,0 +1,32 @@
+# Time = O(n+m) time | O(m) space where n = len(string) and m = len(substring)
+def knuthMorrisPrattAlgorithm(string, substring):
+    pattern = buildPattern(substring)
+	return doesMatch(string, substring, pattern)
+
+def buildPattern(substring):
+	pattern = [-1 for char in substring]
+	j, i = 0, 1
+	while i < len(substring):
+		if substring[i] == substring[j]:
+			pattern[i] = j
+			i += 1
+			j += 1
+		elif j > 0:
+			j = pattern[j-1] + 1
+		else:
+			i += 1
+	return pattern
+
+def doesMatch(string, substring, pattern):
+	i, j = 0, 0
+	while len(substring) - j <= len(string) - i:
+		if string[i] == substring[j]:
+			if j == len(substring) - 1:
+				return True
+			i += 1
+			j += 1
+		elif j > 0:
+			j = pattern[j-1] + 1
+		else:
+			i += 1
+	return False
