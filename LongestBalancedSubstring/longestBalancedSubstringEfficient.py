928d11ec61100f061b8521c277eeab6e0560605d
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-25 04:43:29 -0700

    Update longestBalancedSubstringEfficient.py

--- LongestBalancedSubstring/longestBalancedSubstringEfficient.py
+++ LongestBalancedSubstring/longestBalancedSubstringEfficient.py
@@ -1,3 +1,12 @@
+# Solution idea: The idea is that as we go from left to right or vice versa, the logic still applies. As we go from left to right,
+# we keep counter of the number of opening and closing parenthesis. As we loop through the characters in the string, we increment
+# the corresponding opening or closing parenthesis. Then we check if # of opening equals # of closing parenthesis. If so,
+# we add maxLength = max of maxLength (currently largest seen and initialized to 0) or 2 * (# of closing parenthesis we've seen). 
+# Otherwise, if the number of closing exceeds opening parenthesis, we hit the instance where we can't balance the parenthesis.
+# So reset the counters of opening & closing to 0. The example why we check both left to right and right to left is the following:
+# ((). Note when we go from left to right, we don't update largest balanced bracket.
+
+# Explaination: This follows from two for loops; first goes from left to right and the second goes from right to left. 
 # Time = O(n) | Space = O(1)
 def longestBalancedSubstring(string):
     	leftParenCount = rightParenCount = 0
