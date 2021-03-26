171c7b9a366aa8ef44d0b2eb01af85aa60ee9db5
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-26 04:49:03 -0700

    Update recursionOptimized.py

--- SquareOfZeros/recursionOptimized.py
+++ SquareOfZeros/recursionOptimized.py
@@ -1,3 +1,14 @@
+# Explanation of solution: The idea is to build this matrix where at index (i, j), it'll tell us how many zeros below and to the right (including index (i, j)) until
+# we see a 1. To build this, start with a matrix of the same size as 'matrix' and when we see a 0, add numZerosBelow, numZerosRight to be 1. Otherwise, set it to 1.
+# We build this matrix from last row going backward to first row and from right column all the way backward to first column. At index (i, j), we see that if it
+# corresponds to matrix[i][j] == 1, continue. Otherwise, we add numZerosBelow at index (i+1, j) to index (i, j) in infoMatrix and add numZerosRight at index (i, j+1) to 
+# index (i, j) in infoMatrix provided we can access them (not last row or last column). Once we build this matrix, to check if a square has boundary zeros at key
+# 'r1-r2-c1-c2', we look if infoMatrix[r1][c1]["numZerosBelow"] & infoMatrix[r1][c1]["numZerosRight"] are at least as great as the length of the square. We do the same
+# for infoMatrix[r1][c2]["numZerosBelow"] & infoMatrix[r2][c1]["numZerosRight"] are at least as great as the length of the square.
+
+# Explanation: It's the same as the recursive method (non-Optimize) but checking the boundaries are now O(1) given we have access to number of zeros below and to the right
+# for each index (i, j). Note when we say number of zeros, we mean including the index (i, j) themselves and how many zeros to the right of (i, j) until we see a 1. Same
+# logic with bottom.
 # Time = O(N^3) | Space = O(N^3)
 def squareOfZeroes(matrix):
 	infoMatrix = preComputeNumberOfZeros(matrix)
