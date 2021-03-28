1fcaa4d9ae4abbd9df0737e24518c7970927fe2f
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-03-28 03:03:18 -0700

    Update allKindsOfNodeDepthsSpaceEfficient.py

--- AllKindsOfNodeDepths/allKindsOfNodeDepthsSpaceEfficient.py
+++ AllKindsOfNodeDepths/allKindsOfNodeDepthsSpaceEfficient.py
@@ -1,6 +1,11 @@
-# Solution Explanation:
+# Solution Explanation: We perform a post order traversal of the tree. In this order, given node tree, we build tree.left TreeInfo and tree.right TreeInfo that stores
+# number of nodes in the subtree, the current sum of depths at tree.left & tree.right, and the sum of all sum of depths at each of the nodes in the subtree (left / right). Using
+# these information, we build up these info for the node tree. The number of nodes at tree is tree.left and tree.right number of nodes plus 1. To build tree's sum of depths,
+# we look at tree.left. and tree.right 's sum of depths, we add number of nodes in tree.left and tree.right, respectively. Note that tree.left sum of depths plus number of nodes
+# in left subtree is the sub of depths for tree except the computation for the right subtree. We do the sume for the right subtree. 
 
-# Explanation of complexities:
+# Explanation of complexities: Space comes from the call stack. The time complexity follows from visiting each node at each step of the post order traversal and doing constant work.
+
 # Time = O(N) | Space = O(d) where d is the depth of the tree.
 def allKindsOfNodeDepths(root):
     	return getTreeInfo(root).totalSumOfDepths
