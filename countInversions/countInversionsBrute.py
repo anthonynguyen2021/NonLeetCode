54e59a3d104ddf5787ce21d47f9f5321ef2560c8
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-04-03 23:12:47 -0700

    Update countInversionsBrute.py

--- countInversions/countInversionsBrute.py
+++ countInversions/countInversionsBrute.py
@@ -1,8 +1,8 @@
 # Time = O(n^2) | Space = O(1)
 def countInversions(array):
-    numInversion = 0
+    	numInversion = 0
 	for i in range(0, len(array)):
 		for j in range(i+1, len(array)):
 			if array[i] > array[j]:
 				numInversion += 1
-    return numInversion
+    	return numInversion
