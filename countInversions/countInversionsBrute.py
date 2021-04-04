777442f96f4f4772ef918bb580031a642767d301
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-04-04 01:36:03 -0700

    Update countInversionsBrute.py

--- countInversions/countInversionsBrute.py
+++ countInversions/countInversionsBrute.py
@@ -1,3 +1,5 @@
+# Explanation of solution: For each pair of indices i, j with i < j (nested for loop - see below), we check if array[i] > array[j]. If so, increment the count.
+
 # Time = O(n^2) | Space = O(1)
 def countInversions(array):
     	numInversion = 0
