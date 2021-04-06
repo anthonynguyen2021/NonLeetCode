bbb18e89aa4156c56c4e5f2149f403083bfaf427
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-04-06 00:40:59 -0700

    Update maxSumSubmatrice.py

--- MaximumSumSubmatrix/maxSumSubmatrice.py
+++ MaximumSumSubmatrix/maxSumSubmatrice.py
@@ -1,8 +1,17 @@
-# Brute force idea:
+# Brute force idea: Similar to the nested forloop in the main function, we sum of the elements of the squares, so the time complexity is O( w * h * size * size) with O(1) space.
 
-# Explanation of solution:
+# Explanation of solution: If we have a matrix called sumsMatrix such that sumsMatrix[i][j] will sum all elements in matrix[:i+1][:j+1] (not Python notation), we can
+# compute all of the square submatrices sum in O(1) time. To create sumsMatrix, we first initialize sumsMatrix[0][0] = matrix[0][0]. For the first row,
+# we set sumsMatrix[0][j] = sumsMatrix[0][j-1] + matrix[0][j] and the first column as sumsMatrix[i][0] = sumsMatrix[i-1][0] + matrix[i][0]. To populate the non-left/top wall
+# of the matrix, we set sumsMatrix[i][j] = matrix[i][j] + sumsMatrix[i][j-1] + sumsMatrix[i-1][j] - sumsMatrix[i-1][j-1] (draw a picture to see). Now to the main function,
+# if our square doesn't touch the left and top wall, we have our formula will do the following for the sum of the square: sumsMatrix[row][col] - sumsMatrix[row][col - size] 
+# - sumsMatrix[row - size][col] + sumsMatrix[row - size][col - size] (draw a picture to see). Note that if the square we're considering touches the left wall and not the top wall, 
+# we have the following formula for the square: sumsMatrix[row][col] - sumsMatrix[row - size][col]. If our square touches the top but not the left wall, the sum of the
+# square elements is sumsMatrix[row][col] - sumsMatrix[row][col - size]. If our square touches the left AND top wall, the sum of the squares is just sumsMatrix[row][col]. Note
+# that the squares we're considering will have bottom right hand corner index (row, col) in Python indexing - see code below.
 
-# Explanation of complexities:
+# Explanation of complexities: The matrix sumsMatrix is build using O(w * h) space. For time, it takes O(w * h) to build sumsMatrix. Worst case, we might have O(w * h) square 
+# submatrices. But the nested loop is approximately O(w * h) where the work inside is O(1).
 # Time = O(w * h) | Space = O(w * h) where w is the width and h is the height
 def maximumSumSubmatrix(matrix, size):
     	sumsMatrix = getTotalSumMatrix(matrix)
