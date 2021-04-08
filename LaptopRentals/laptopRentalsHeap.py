c101dd4e411850575859b4bdc545a8dc8dbf6082
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-04-07 18:30:08 -0700

    Create laptopRentalsHeap.py

---
+++ LaptopRentals/laptopRentalsHeap.py
@@ -0,0 +1,19 @@
+import heapq
+
+# Time = O(nlog n) | Space = O(n)
+def laptopRentals(times):
+	if len(times) == 0:
+		return 0
+	sortedTimes = sorted(times, key = lambda x : x[0])
+	buildHeap = [sortedTimes[0][1]]
+    minLaptop = 1
+	for idx in range(1, len(sortedTimes)):
+		currentRental = sortedTimes[idx]
+		currentRentalStart, currentRentalEnd = currentRental
+		if currentRentalStart < buildHeap[0]:
+			heapq.heappush(buildHeap, currentRentalEnd)
+			minLaptop = max(minLaptop, len(buildHeap))
+		else:
+			heapq.heappop(buildHeap)
+			heapq.heappush(buildHeap, currentRentalEnd)
+    return minLaptop
