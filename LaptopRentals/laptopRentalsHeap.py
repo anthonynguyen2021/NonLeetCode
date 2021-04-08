390ac8185ea260f874d553d5bf3335c2a98110b0
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-04-07 19:28:58 -0700

    Update laptopRentalsHeap.py

--- LaptopRentals/laptopRentalsHeap.py
+++ LaptopRentals/laptopRentalsHeap.py
@@ -1,5 +1,13 @@
 import heapq
 
+# Explanation of solution: We sort by the first coordinate of times. As we loop through the sorted time from left to right for idx in {0, ... , len(times)-1}, if 
+# sortedTimes[idx][0] < heap[0] where the heap stores the ending time and the top of the heap stores the next person to return the laptop, we push in 
+# sortedTimes[idx][1] into the heap and keep track of the size of the heap; this boolean check means that the next person to rent out the laptop rents it out before the next
+# person returns it. If sortedTimes[idx][0] >= heap[0], this means the next person who returns the laptop does so before the next person rents out the laptop. In this case,
+# we don't increment the count. We pop out and push in sortedTimes[idx][1] into the heap.
+
+# Explanation of complexities: The space comes from the heap storage. The space comes from sorting times array - O(nlogn) - and n insertion / pop from the heap (each are at most
+# log n), so O(nlogn).
 # Time = O(nlog n) | Space = O(n)
 def laptopRentals(times):
 	if len(times) == 0:
