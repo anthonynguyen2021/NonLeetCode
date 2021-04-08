91779767c61c1603ee5302d8f23454905ac18df4
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-04-07 18:30:31 -0700

    Update laptopRentalsHeap.py

--- LaptopRentals/laptopRentalsHeap.py
+++ LaptopRentals/laptopRentalsHeap.py
@@ -6,7 +6,7 @@ def laptopRentals(times):
 		return 0
 	sortedTimes = sorted(times, key = lambda x : x[0])
 	buildHeap = [sortedTimes[0][1]]
-    minLaptop = 1
+    	minLaptop = 1
 	for idx in range(1, len(sortedTimes)):
 		currentRental = sortedTimes[idx]
 		currentRentalStart, currentRentalEnd = currentRental
@@ -16,4 +16,4 @@ def laptopRentals(times):
 		else:
 			heapq.heappop(buildHeap)
 			heapq.heappush(buildHeap, currentRentalEnd)
-    return minLaptop
+	return minLaptop
