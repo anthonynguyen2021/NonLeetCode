c20b9de2c6f4e7c50165c6fec191a5172e27547e
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-06-13 00:16:42 -0700

    Create minNumberOfCoinsForChange.py

---
+++ MinNumberOfCoinsForChange/minNumberOfCoinsForChange.py
@@ -0,0 +1,11 @@
+# Time = O(d * n) | Space = O(n)
+# Idea of solution: Use DP. Build the solution up by having
+# only denoms[0], ..., denoms[j] for 0 <= j < len(denoms).
+def minNumberOfCoinsForChange(n, denoms):
+  minCoins = [float('inf') for _ in range(n+1)]
+	minCoins[0] = 0
+	for denom in denoms:
+		for amount in range(0, n+1):
+			if denom <= amount:
+				minCoins[amount] = min(minCoins[amount], 1 + minCoins[amount-denom])
+	return -1 if minCoins[-1] == float('inf') else minCoins[-1]		
