fdbdc3100c235c7a5ad811ec3bc0f445d9a26224
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-06-16 13:56:49 -0700

    Update continuousMedian.py

--- ContinuousMedian/continuousMedian.py
+++ ContinuousMedian/continuousMedian.py
@@ -15,3 +15,24 @@ class ContinuousMedianHandler:
 
     def getMedian(self):
         return self.median
+
+import heapq
+
+class ContinuousMedianHandler:
+    def __init__(self):
+        self.max_heap = []
+	self.min_heap = []
+        self.median = None
+
+    def insert(self, number):
+    	if len(self.max_heap) == len(self.min_heap):
+		# We can't just push into max heap. Here's an example: max_heap = [7], min_heap = [10] and we insert 11 into the max heap. 
+		# So we push and pop in the min heap to ensure the pop element is less than or equal to the min heap and push the popped element into the max heap.
+		heapq.heappush(self.max_heap, -heapq.heappushpop(self.min_heap, number))
+		self.median = -self.max_heap[0]
+	else:
+		heapq.heappush(self.min_heap, -heapq.heappushpop(self.max_heap, -number))
+		self.median = 0.5 * (-self.max_heap[0] + self.min_heap[0])
+		
+    def getMedian(self):
+        return self.median
