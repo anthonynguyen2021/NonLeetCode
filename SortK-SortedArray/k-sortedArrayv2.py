09dc2e49412455ebf9818ca94acfa92adc05d829
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-06-22 18:27:41 -0700

    Update k-sortedArrayv2.py

--- SortK-SortedArray/k-sortedArrayv2.py
+++ SortK-SortedArray/k-sortedArrayv2.py
@@ -1,9 +1,11 @@
 import heapq
 # Time = O(k + nlogk) | Space = O(k)
+# Explanation: Push in k elements into the heap and heapify. Look through the remaining elements in array. If for loop doesn't execute, then we have only k elements. Otherwise,
+# push in and get the minimum element since position k (python indexing) can belong to index 0.
 def sortKSortedArray(array, k):
-  if not array or k == 0:
-	  return array
-	minHeap = array[:min(k, len(array))]
+	if not array or k == 0:
+		return array
+	minHeap = array[:min(k, len(array))] # For error checking if k >= len(array)
 	heapq.heapify(minHeap)
 	insertIdx = 0
 	for idx in range(min(k, len(array)), len(array)):
