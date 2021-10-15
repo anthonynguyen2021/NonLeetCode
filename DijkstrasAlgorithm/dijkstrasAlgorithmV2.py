5ce81624a643964f9ee26f4161fb5b412005e74d
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2021-10-14 20:19:44 -0700

    Update dijkstrasAlgorithmV2.py

--- DijkstrasAlgorithm/dijkstrasAlgorithmV2.py
+++ DijkstrasAlgorithm/dijkstrasAlgorithmV2.py
@@ -1,7 +1,13 @@
+# Time = O(V+E) | Space = O(V) where V, E are vertices and edges, respectively. This is the classifical BFS algorithm.
+
+# Idea of solution: Start the BFS at the start node. Pop off the front of the queue and for all the neighboring nodes, update the neighboring node's smallest distance
+# the smallest distance of the current's node + distance from current to neighboring node (take the min with neighboring's node smallest distance). If neighboring node 
+# has not been visited, push into queue, add to visit set. At the end, map all infinities to -1.
+
 from collections import deque
 
 def dijkstrasAlgorithm(start, edges):
-    result = len(edges) * [float('inf')]
+    	result = len(edges) * [float('inf')]
 	result[start] = 0
 	visited = set([start])
 	
