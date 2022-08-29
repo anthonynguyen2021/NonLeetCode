c2a4b1abd1d5ac6afc5e85db46c8ef73895cc775
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2022-08-29 15:35:27 -0700

    Create award_budget_cuts.py

---
+++ Award_Budget_Cuts_Pramp/award_budget_cuts.py
@@ -0,0 +1,21 @@
+# time = O(nlogn) | space = O(n) where n = len(grantsArray)
+# solution: sort grantsArray in reverse order. Define surplus = sum(grantsARray) - newBudget. If surplus is negative, that means we have a lot of budgets, so no cuts. So set cap = grantsArray[0]. Let's assume that newBudget is not < 0. Imagine stacking these budgets as bar graphs. Then we subtrack the highest high and width. If surplus <= 0, break. The moment we break, the answer is gransArray[idx] + abs(surplus) / float(idx) - draw a picture.
+def find_grants_cap(grantsArray, newBudget):
+
+  grantsArray.sort(reverse=True)
+  
+  surplus = sum(grantsArray) - newBudget
+  
+  if surplus <= 0:
+    return grantsArray[0]
+  
+  grantsArray.append(0)
+  
+  for idx in range(1, len(grantsArray)):
+    
+    surplus -= (grantsArray[idx-1] - grantsArray[idx]) * idx
+    
+    if surplus <= 0:
+      break
+      
+  return grantsArray[idx] + (abs(surplus) / float(idx))
