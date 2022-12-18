5c4aed980d81504ecfcbf3af41849664261dce62
Anthony <antngu@ucdavis.edu>
2022-12-17 17:57:53 -0800

    Reformatting

--- AddTwoLinkedList/AddTwo.py
+++ AddTwoLinkedList/AddTwo.py
@@ -1,15 +1,18 @@
 # Definition for singly-linked list.
+
 # class ListNode:
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
     # Time = O(n) where n is the largest size of both linked list
     # Space = O(n) since in the worst case, we may have n function calls in the call stack
+
     # Idea: Use a helper method that keeps track of the carry. Note the carry is at most 1 since a number in S = {1, ..., 9} plus another number in S yields a number at most 18, 
     # so a carry of at most 1. We implement a recursive method. So at each pair of nodes from the linked list and a carry, we add the values and add the ones digit in a new node
     # and keep track of the carry. Call the function again on the next ndoes of l1 and l2. 
-    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+    def addTwoNumbers(self, l1: 'ListNode', l2: 'ListNode') -> 'ListNode':
         return self.helperAddTwoNumbers(l1, l2, 0)
     
     def helperAddTwoNumbers(self, l1, l2, carry):
--- AddTwoLinkedList/addTwoEfficient.py
+++ AddTwoLinkedList/addTwoEfficient.py
@@ -1,4 +1,5 @@
 # This is an input class. Do not edit.
+
 class LinkedList:
     def __init__(self, value):
         self.value = value
@@ -11,23 +12,24 @@ class LinkedList:
 
 # Time = O(max(m, n)) = Space where m = length of first linked list and n is the length of second.
 def sumOfLinkedLists(linkedListOne, linkedListTwo):
-    	prev = prevHead = LinkedList(0)  # prev, prevHead points to the same class object
+
+	prev = prevHead = LinkedList(0)  # prev, prevHead points to the same class object
 	carry = 0
 	left, right = linkedListOne, linkedListTwo
-	
-  # The trick to getting this while loop to work simplifies the code and set leftVal / rightVal to 0 if it is None.
+
+	# The trick to getting this while loop to work simplifies the code and set leftVal / rightVal to 0 if it is None.
 	while left or right or carry != 0:
+
 		leftVal = left.value if left else 0
 		rightVal = right.value if right else 0
 		addVal = leftVal + rightVal + carry
-		
+
 		carry = addVal // 10
 		ones = addVal % 10
-		
+
 		prev.next = LinkedList(ones)
 		prev = prev.next
 		left = left.next if left else None  # Nice use of ternary operator in case we have the case when left is None in which we do nothing; otherwise, we advance left by one.
 		right = right.next if right else None
-		
+
 	return prevHead.next
-		
