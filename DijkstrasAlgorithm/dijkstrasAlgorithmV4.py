fa262201ef15e0a9359d9741adfd868d82943d96
Anthony Nguyen <64521273+anthonynguyen2021@users.noreply.github.com>
2023-05-05 12:23:10 -0700

    Update dijkstrasAlgorithmV4.py

--- DijkstrasAlgorithm/dijkstrasAlgorithmV4.py
+++ DijkstrasAlgorithm/dijkstrasAlgorithmV4.py
@@ -21,7 +21,7 @@ def dijkstrasAlgorithm(start, edges):
 
 	while len(minHeap) > 0:
 
-		currentDistance, node = heapq.heappop(minHeap) # currentDistance is the shortest distance from start to node (assuming node hasn't been visited)
+		currentDistance, node = heapq.heappop(minHeap)  # currentDistance is the shortest distance from start to node (assuming node hasn't been visited)
 
 		if node in visited:
 			continue
